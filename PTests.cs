030b7f0b666722be5178eefa052ac93563a6a5f0
Thyago Suzuki <90354921+suzukiil@users.noreply.github.com>
2024-10-09 11:12:59 -0300

    Add files via upload

---
+++ PTests.cs
@@ -0,0 +1,41 @@
+﻿public class IntegrationTests : BaseIntegrationTest
+{
+    public IntegrationTests(IntegrationTestWebAppFactory factory)
+        : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Create_ShouldThrowArgumentException_WhenSkuIsInvalid()
+    {
+        var command = new CreateProductCommand("mydb", "123", "blabla");
+
+        Task Action() => Sender.Send(command);
+ 
+        await Assert.ThrowsAsync<ArgumentException>(Action);
+    }
+
+    [Fact]
+    public async Task Create_ShouldAddProduct_WhenCommandIsValid()
+    {
+        var command = new CreateCommand("mydb", "123", "blabla");
+
+        var PId = await Sender.Send(command);
+
+        var person = DbContext.Person.FirstOrDefault(p => p.Id == new PId(pId));
+
+        Assert.NotNull(person);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnPerson_WhenPersonExists()
+    {
+        var command = new CreateCommand("mydb", "123", "blabla");
+        var PId = await Sender.Send(command);
+        var query = new GetPersonQuery(new PId(PId));
+
+        var person = await Sender.Send(query);
+
+        Assert.NotNull(person);
+    }
+}
